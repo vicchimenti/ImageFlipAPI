cdb187846f4270d1311df4aefe87445a6183cbd4
Victor Chimenti <chimenti@seattleu.edu>
2019-03-12 07:28:25 -0700

    class write helper

---
+++ ImageWriter/Helper/WriteHelper.cs
@@ -0,0 +1,20 @@
+namespace ImageWriter.Helper
+{
+    public class WriteHelper
+    {
+        public enum ImageFormat
+        {
+            bmp,
+            jpeg,
+            gif,
+            tiff,
+            png,
+            unknown
+        }
+
+        public static GetImageFormat(byte[] bytes)
+        {
+            
+        }
+    }
+}

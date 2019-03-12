191fc6eaa8c3b8b0069d16d14670b53b6508b6ac
Vic Chimenti <chimenti@seattleu.edu>
2019-03-12 09:50:55 -0700

    image handler

--- Handler/ImageHandler.cs
+++ Handler/ImageHandler.cs
@@ -1,7 +1,30 @@
-namespace ImageFlipAPI.Handler
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ImageFlipAPI.ImageWriter.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageUploader.Handler
 {
-    public class ImageHandler
+    public interface IImageHandler
     {
-        
+        Task<IActionResult> UploadImage(IFormFile file);
+    }
+
+    public class ImageHandler : IImageHandler
+    {
+        private readonly IImageWriter _imageWriter;
+        public ImageHandler(IImageWriter imageWriter)
+        {
+            _imageWriter = imageWriter;
+        }
+
+        public async Task<IActionResult> UploadImage(IFormFile file)
+        {
+            var result = await _imageWriter.UploadImage(file);
+            return new ObjectResult(result);
+        }
     }
 }
